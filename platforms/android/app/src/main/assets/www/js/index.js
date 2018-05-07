cb1ed6c9ae1043b76efee888e57194076c8b447a
Nazmul Hossain <gmnazmul@deligram.com>
2018-05-07 13:00:03 +0600

    redirect to app url

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -28,6 +28,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+         window.location="https://www.deligram.com";
     },
 
     // Update DOM on a Received Event
